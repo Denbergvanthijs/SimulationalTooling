e7e6223950a36b8b232a5bc10acb1a4596cb209c
denbergvanthijs <denbergvanthijs@gmail.com>
2019-11-20 10:12:50 +0100

    Auto's rijden niet meer door rood

--- 3_ClassesInterupt.py
+++ 3_ClassesInterupt.py
@@ -4,52 +4,58 @@ import simpy
 def main(tijd):
     env = simpy.Environment()
 
-    car = Car(env)
-    TL = TrafficLight(env, car)
+    TL = TrafficLight(env)
+    car = Car(env, TL)
 
     env.run(until=tijd)
     print(f"Simulation gestopt op: {tijd}")
 
 
 class TrafficLight(object):
-    def __init__(self, env, other, groen_duur=30, geel_duur=5, rood_duur=20):
+    def __init__(self, env, groen_duur=30, geel_duur=5, rood_duur=20):
         self.env = env
-        self.other = other
 
         self.groen_duur = groen_duur
         self.geel_duur = geel_duur
         self.rood_duur = rood_duur
+        self.staat = 0
 
         self.action = env.process(self.run())
 
     def run(self):
         while True:
             print(f"\x1b[0;32mGroen:{self.env.now:>20}")
+            self.staat = 1
             yield self.env.process(self.licht(self.groen_duur))
 
             print(f"\x1b[0;33mGeel:{self.env.now:>20}")
+            self.staat = 2
             yield self.env.process(self.licht(self.geel_duur))
 
             print(f"\x1b[0;31mRood:{self.env.now:>20}")
+            self.staat = 0
             yield self.env.process(self.licht(self.rood_duur))
-            # self.other.action.interrupt()
 
     def licht(self, duration):
         yield self.env.timeout(duration)
 
 
 class Car(object):
-    def __init__(self, env, duur=10):
+    def __init__(self, env, other, duur=10):
         self.env = env
         self.duur = duur
+        self.other = other
 
         self.action = env.process(self.run())
 
     def run(self):
         while True:
             try:
-                print(f"\x1b[0mRijden:{self.env.now:>20}")
-                yield self.env.process(self.rijtijd(self.duur))
+                if self.other.staat:
+                    print(f"\x1b[0mRijden:{self.env.now:>20}")
+                    yield self.env.process(self.rijtijd(self.duur))
+                else:
+                    yield self.env.process(self.rijtijd(self.duur))
             except simpy.Interrupt:
                 pass
 
